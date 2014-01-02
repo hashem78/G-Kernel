aedcef6eca9e1adc7c1c847c62e8bf4648d98d4b
Andre Saddler <dresadd09691@gmail.com>
2014-01-01 23:48:35 -0500

    Add OndemandPlus gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -431,6 +431,13 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass2)
 
+
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandplus)
 #endif
 
 
